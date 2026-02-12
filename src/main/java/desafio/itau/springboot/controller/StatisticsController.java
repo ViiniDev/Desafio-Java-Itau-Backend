cb17465dbaf7fe533a07e6ad976f3ea72791234f
ViiniDev <viniciusarruda189@gmail.com>
2026-02-12 16:08:51 -0300

    controller statistics completa

--- src/main/java/desafio/itau/springboot/controller/StatisticsController.java
+++ src/main/java/desafio/itau/springboot/controller/StatisticsController.java
@@ -1,2 +1,13 @@
-public class TransactionController {
+@RestController
+@RequestMapping ("/estatisticas")
+public class StatisticsController {
+    private final transactionService transactionService;
+    public StatisticsController(transactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+    @GetMapping
+    public ResponseEntity<StatisticsDTO> getStatistics() {
+        DoubleSummaryStatistics stats = transactionService.getStatistics();
+        return ResponseEntity.ok(new StatisticsDTO(stats));
+    }
 }
