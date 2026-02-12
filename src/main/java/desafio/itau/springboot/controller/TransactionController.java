d4f217a072ebe36e2b0784f144fbd6ca3e494960
ViiniDev <viniciusarruda189@gmail.com>
2026-02-12 16:05:34 -0300

    controller transaction corrijido com retorno correto

--- src/main/java/desafio/itau/springboot/controller/TransactionController.java
+++ src/main/java/desafio/itau/springboot/controller/TransactionController.java
@@ -7,7 +7,7 @@ public class TransactionController {
 
     @PostMapping
     public ResponseEntity<TransactionController> createTransaction(@Valid @RequestBody TransactionController transactionController) {
-        if(transactionController.getDataHora().isAfter(OffsetDateTime.now())){
+        if(transactionController.getDataHora().isAfter(OffsetDateTime.now()) || transactionController.getValor() <= 0) {
             return ResponseEntity.unprocessableEntity().build();
         }
         transactionService.addTransaction(new Transaction(transactionController.getValor(), transactionController.getDataHora()));
